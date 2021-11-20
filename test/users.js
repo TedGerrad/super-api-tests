5ee08a2347bd50b5c84e8753026b6466fc7b569a
TedGerrad <tedariaga@gmail.com>
2021-11-20 00:59:47 +0000

    This commit contains positive and negative test scenarios for a GET request using Chai assertions, asynchronus calls and a "done()" callback function. Timeout is also appended in package.json to enable the callback function run before the test passes or fails

--- test/users.js
+++ test/users.js
@@ -1,13 +1,31 @@
 import supertest from "supertest";
 const request = supertest('https://gorest.co.in/public-api/'); 
+
+import { expect } from "chai";
+/*
+By importing assertions from the chai library, we can remove "console.log"
+from our original request
+*/
+
+
 const TOKEN = '9090ceb54777b92ad8627cd78cf83c97e08447abbc7ac2cde123e05c2d776bf6';
 
+//POSITIVE test scenario that SHOULD PASS
 describe('Users', ()=> {
-    it('GET /users', () => {
-        request
-            .get('users?access-token=${TOKEN}').end((err,res) => {
-                console.log(err);
-                console.log(res.body);
-            })
+    it('GET /users', (done) => {
+        request.get('users?access-token=${TOKEN}').end((err,res) => {
+           expect(res.body.data).to.not.be.empty;   
+           done();  
+        })
+    });
+})
+
+//NEGATIVE test scenario that SHOULD FAIL
+describe('Users', ()=> {
+    it('GET /users', (done) => {
+        request.get('users?access-token=${TOKEN}').end((err,res) => {
+           expect(res.body.data).to.be.empty;   
+           done();  
+        })
     });
 })
